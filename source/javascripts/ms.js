a17e35bc9a89bd265e708d78bb1cc38c6371739d
Scott Kellum <scott@scottkellum.com>
2014-04-14 10:19:27 -0400

    Tweaking some error handling here.

--- source/javascripts/ms.js
+++ source/javascripts/ms.js
@@ -1,4 +1,22 @@
-function ms(msValue, msBases, msRatios) {
+var msValue = 0;
+var msBases = 1;
+var msRatios = (1+ Math.sqrt(5))/2;
+
+function ms(value, bases, ratios) {
+
+  if (typeof value === 'string') {
+    value = 1;
+  }
+  if (value == undefined) {
+    value = msValue;
+  }
+  if (bases == undefined) {
+    bases = msBases;
+  }
+  if (ratios == undefined) {
+    ratios = msRatios;
+  }
+
   // (r^v)*b
-  return Math.pow(msRatios, msValue) * msBases;
+  return Math.pow(ratios, value) * bases;
 }
